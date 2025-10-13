61ade446cbfebd986c9d373eea79a976fd1d7c53
tuhinx <20234208032@eee.bubt.edu.bd>
2025-10-13 13:52:07 +0600

    [Added] Update

--- Learn/class_20/lib/main.dart
+++ Learn/class_20/lib/main.dart
@@ -1,122 +1,173 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  List<int> myList = [];
+  int index = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    // TODO: implement initState
+    for (int i = 1; i < 50; i++) {
+      myList.add(i);
+    }
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+        title: Text("ListView Class", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+      body: buidlistViews(),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.view_list),
+            label: "ListView",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_rounded),
+            label: "Vertical",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.line_style),
+            label: "Horizontal",
+          ),
+        ],
+        onTap: (int index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget? buidlistViews() {
+    if (index == 0) {
+      return listViews();
+    } else if (index == 1) {
+      return verticalListView();
+    } else {
+      return horizontalListView();
+    }
+  }
+
+  Widget? listViews() {
+    return ListView(
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text("Ak", style: TextStyle(color: Colors.white)),
+          ),
+          title: Text("Ab Karim"),
+          subtitle: Text("Flutter Dev"),
+          trailing: Icon(Icons.star, color: Colors.orange),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text("Bk", style: TextStyle(color: Colors.white)),
+          ),
+          title: Text("Bb Karim"),
+          subtitle: Text("Flutter Dev"),
+          trailing: Icon(Icons.star, color: Colors.orange),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text("Ck", style: TextStyle(color: Colors.white)),
+          ),
+          title: Text("Cb Karim"),
+          subtitle: Text("Flutter Dev"),
+          trailing: Icon(Icons.star, color: Colors.orange),
+        ),
+        ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Text("Dk", style: TextStyle(color: Colors.white)),
+          ),
+          title: Text("Db Karim"),
+          subtitle: Text("Flutter Dev"),
+          trailing: Icon(Icons.star, color: Colors.orange),
+        ),
+      ],
+    );
+  }
+
+  // For Vertical ListView
+  Widget? verticalListView() {
+    return ListView.separated(
+      separatorBuilder: (context, index) {
+        return Divider(color: Colors.grey);
+      },
+      itemCount: myList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.red,
+            child: Text(
+              "${myList[index]}",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          title: Text("Ab Karim"),
+          subtitle: Text("Flutter Dev"),
+          trailing: Icon(Icons.star, color: Colors.orange),
+        );
+      },
+    );
+  }
+
+  // For Horizontal ListView
+  Widget? horizontalListView() {
+    return ListView.separated(
+      padding: EdgeInsets.all(16),
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (context, index) {
+        return Container(
+          width: 100,
+          height: 50,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Text(
+                "${myList[index]}",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => Divider(color: Colors.grey),
+      itemCount: myList.length,
     );
   }
 }
