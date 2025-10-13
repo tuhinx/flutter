1fecb9ca6641df0a9846bb423d78b359659131e4
tuhinx <20234208032@eee.bubt.edu.bd>
2025-10-14 01:32:13 +0600

    [Added] Update

---
+++ Learn/class_22/android/app/src/main/kotlin/io/github/tuhinx/class22/class_22/MainActivity.kt
@@ -0,0 +1,5 @@
+package io.github.tuhinx.class22.class_22
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Learn/class_22/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  List<int> myLists = [];
+
+  @override
+  void initState() {
+    for (int i = 0; i < 10; i++) {
+      myLists.add(i);
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'GridView Count Class',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.orange,
+            alignment: Alignment.center,
+            child: Text("1", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.orange,
+            alignment: Alignment.center,
+            child: Text("1", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.orange,
+            alignment: Alignment.center,
+            child: Text("1", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.orange,
+            alignment: Alignment.center,
+            child: Text("1", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.orange,
+            alignment: Alignment.center,
+            child: Text("1", style: TextStyle(fontSize: 30)),
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            color: Colors.orange,
+            alignment: Alignment.center,
+            child: Text("1", style: TextStyle(fontSize: 30)),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
